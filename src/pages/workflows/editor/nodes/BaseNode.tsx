318a1fe2ea4eb6cb45875bf617776a553ad8e788
Kostiantyn Pshenychnyi1 <kostiantyn_pshenychnyi1@epam.com>
2026-04-27 08:49:11 +0000

    EPMCDME-11901: Fix action buttons hide iterator node

--- src/pages/workflows/editor/nodes/BaseNode.tsx
+++ src/pages/workflows/editor/nodes/BaseNode.tsx
@@ -17,6 +17,7 @@ import React, { useContext } from 'react'
 
 import Check18Svg from '@/assets/icons/check-18.svg?react'
 import CrossSvg from '@/assets/icons/cross.svg?react'
+import StatusBadge from '@/components/StatusBadge'
 import WorkflowExecutionStateControls from '@/pages/workflows/details/states/WorkflowExecutionStateControls'
 import { WorkflowExecutionStatus } from '@/types/entity'
 import { cn } from '@/utils/utils'
@@ -43,21 +44,24 @@ const BaseNode: React.FC<BaseNodeProps> = ({
   selected = false,
   hasError = false,
   status,
-  success = 0,
-  failures = 0,
+  success,
+  failures,
   active = true,
 }) => {
   const ctx = useContext(ExecutionContext)
 
+  const isIteratorNode = success !== undefined || failures !== undefined
+
   return (
     <div
       className={cn(
-        'bg-surface-base-chat w-64 rounded-xl shadow-md border-[1.5px] relative',
+        'bg-surface-base-chat group/base-node w-64 rounded-xl shadow-md border-[1.5px] relative ',
         'border-border-specific-node-border text-xs transition-all',
         {
           'border-border-specific-node-border-focus': selected,
           'outline outline-offset-2 outline-failed-secondary': hasError,
           'border-transparent': status,
+          '!pointer-events-auto cursor-grab': isIteratorNode,
         },
         getStatusBorderClass(status),
         classNames
@@ -83,14 +87,19 @@ const BaseNode: React.FC<BaseNodeProps> = ({
 
       {children}
 
-      {success > 0 || failures > 0 ? (
-        <div className="flex gap-6 absolute -bottom-6 left-5">
-          {success > 0 ? (
+      {isIteratorNode ? (
+        <div
+          className={cn(
+            'flex gap-6 absolute -bottom-6 left-5',
+            status === 'Interrupted' && 'group-hover/base-node:opacity-0'
+          )}
+        >
+          {(success ?? 0) > 0 ? (
             <div className="flex gap-1 text-success-primary">
               <Check18Svg className="size-4" /> {success}
             </div>
           ) : null}
-          {failures > 0 ? (
+          {(failures ?? 0) > 0 ? (
             <div className="flex gap-1 text-failed-primary">
               <CrossSvg className="size-4" /> {failures}
             </div>
@@ -98,12 +107,26 @@ const BaseNode: React.FC<BaseNodeProps> = ({
         </div>
       ) : null}
 
+      {isIteratorNode && status === 'Interrupted' && ctx?.interruptedStateId && (
+        <div className="flex gap-6 absolute -bottom-6 right-0">
+          <StatusBadge status="pending" text="Action required" />
+        </div>
+      )}
+
       {status === 'Interrupted' && ctx?.interruptedStateId && (
-        <WorkflowExecutionStateControls
-          small
-          stateId={ctx?.interruptedStateId}
-          className="absolute -bottom-14 left-0 pointer-events-auto"
-        />
+        <div
+          className={cn(
+            'absolute top-full left-0 pointer-events-auto',
+            isIteratorNode &&
+              'flex items-end opacity-0 h-0 overflow-hidden group-hover/base-node:h-auto pointer-events-none group-hover/base-node:opacity-100 group-hover/base-node:pointer-events-auto hover:pointer-events-auto'
+          )}
+        >
+          <WorkflowExecutionStateControls
+            small
+            stateId={ctx?.interruptedStateId}
+            className="mt-2.5"
+          />
+        </div>
       )}
     </div>
   )
